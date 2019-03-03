e370608655df15c83ea454ef49e1b4474b6233c8
Konrad <krobaszynski@edu.cdv.pl>
2019-03-03 21:42:23 +0100

    add boardgames list to boardgames activity

--- app/src/main/java/com/konradmikolaj/boardgamelibraryclient/BoardgamesActivity.kt
+++ app/src/main/java/com/konradmikolaj/boardgamelibraryclient/BoardgamesActivity.kt
@@ -5,10 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.boardgames.*
 
 
 class BoardgamesActivity : Activity() {
+
+    private val boardgames: ArrayList<String> = ArrayList()
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -33,11 +36,25 @@ class BoardgamesActivity : Activity() {
         setContentView(R.layout.boardgames)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigation.selectedItemId = R.id.navigation_boardgames
+
+        addSampleBoardgames()
+        boardgamesList.layoutManager = LinearLayoutManager(this)
+        boardgamesList.adapter = BoardgameAdapter(boardgames, this)
     }
 
     companion object {
         fun newIntent(context: Context): Intent {
             return Intent(context, BoardgamesActivity::class.java)
         }
+    }
+
+    fun addSampleBoardgames(){
+        boardgames.add("Osadnicy z Catanu")
+        boardgames.add("Przykladowa Gra")
+        boardgames.add("Magia i Miecz")
+        boardgames.add("Splendor")
+        boardgames.add("Talisman")
+        boardgames.add("Domek")
+        boardgames.add("Napad bandziorów na krzak pomidorów")
     }
 }
